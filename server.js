259475229a07e9a8377518c60c5e6827939066eb
stanleyyork <stanleyyork@gmail.com>
2015-12-11 10:52:48 -0800

    fixing id for index

--- server.js
+++ server.js
@@ -106,7 +106,7 @@ app.get('/logout', function (req, res) {
 
 // GET - Index (Primary Dashboard View)
 app.get('/index', function (req, res){
-  var userId = "5660a6c810d090e34c47938f" //req.user.id; 
+  var userId = req.user.id; 
   User.findOne({_id: userId})
       .populate('activities')
           .exec(function(err, singleUser){
