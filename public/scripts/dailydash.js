059ed388dc6ec5bd5632306349b9f0d41d79e014
stanleyyork <stanleyyork@gmail.com>
2016-03-08 17:59:47 -0800

    redesign of daily dash

--- public/scripts/dailydash.js
+++ public/scripts/dailydash.js
@@ -28,57 +28,57 @@ $(function() {
 				for(var x = 0; x<anaerobic_count; x++){
 					$('#anaerobic-count').append('<span class="glyphicon glyphicon-ok-circle" id="daily-dash-glyph"></span>');
 				}
-				if(today_number-anaerobic_count === 0){
-					$('#anaerobic-count-no').append('<span class="glyphicon glyphicon-remove-circle" id="daily-dash-glyph-hidden"></span>');
-				}
+				// if(today_number-anaerobic_count === 0){
+				// 	$('#anaerobic-count-no').append('<span class="glyphicon glyphicon-remove-circle" id="daily-dash-glyph-hidden"></span>');
+				// }
 				for(var x = 0; x<(today_number-anaerobic_count); x++){
-					$('#anaerobic-count-no').append('<span class="glyphicon glyphicon-remove-circle" id="daily-dash-glyph"></span>');
+					$('#anaerobic-count').append('<span class="glyphicon glyphicon-remove-circle" id="daily-dash-glyph"></span>');
 				}
 				for(var x = 0; x<(7-today_number); x++){
 					$('#anaerobic-count').append('<span class="glyphicon glyphicon-question-sign" id="daily-dash-glyph"></span>');
 				}
 				if(data['plans'][i]['stats']['streak'] > 0){
-					$('#anaerobic-today').append('<span class="glyphicon glyphicon-ok" id="daily-dash-glyph"></span>');
+					$('#anaerobic-today').append('<span class="glyphicon glyphicon-ok"></span>');
 				} else {
-					$('#anaerobic-today').append('<span class="glyphicon glyphicon-remove" id="daily-dash-glyph"></span>');
+					$('#anaerobic-today').append('<span class="glyphicon glyphicon-remove"></span>');
 				}
 			} else if(data['plans'][i]['name'] === 'Aerobic') {
 				var aerobic_count = data['plans'][i]['stats']['weekly_progress'][4]['checkin_count'];
 				for(var x = 0; x<aerobic_count; x++){
 					$('#aerobic-count').append('<span class="glyphicon glyphicon-ok-circle" id="daily-dash-glyph"></span>');
 				}
-				if(today_number-aerobic_count === 0){
-					$('#aerobic-count-no').append('<span class="glyphicon glyphicon-remove-circle" id="daily-dash-glyph-hidden"></span>');
-				}
+				// if(today_number-aerobic_count === 0){
+				// 	$('#aerobic-count-no').append('<span class="glyphicon glyphicon-remove-circle" id="daily-dash-glyph-hidden"></span>');
+				// }
 				for(var x = 0; x<(today_number-aerobic_count); x++){
-					$('#aerobic-count-no').append('<span class="glyphicon glyphicon-remove-circle" id="daily-dash-glyph"></span>');
+					$('#aerobic-count').append('<span class="glyphicon glyphicon-remove-circle" id="daily-dash-glyph"></span>');
 				}
 				for(var x = 0; x<(7-today_number); x++){
 					$('#aerobic-count').append('<span class="glyphicon glyphicon-question-sign" id="daily-dash-glyph"></span>');
 				}
 				if(data['plans'][i]['stats']['streak'] > 0){
-					$('#aerobic-today').append('<span class="glyphicon glyphicon-ok" id="daily-dash-glyph"></span>');
+					$('#aerobic-today').append('<span class="glyphicon glyphicon-ok"></span>');
 				} else {
-					$('#aerobic-today').append('<span class="glyphicon glyphicon-remove" id="daily-dash-glyph"></span>');
+					$('#aerobic-today').append('<span class="glyphicon glyphicon-remove"></span>');
 				}
 			} else if(data['plans'][i]['name'] === 'Eat Fruit') {
 				var eat_fruit_count = data['plans'][i]['stats']['weekly_progress'][4]['checkin_count'];
 				for(var x = 0; x<eat_fruit_count; x++){
 					$('#eat-fruit-count').append('<span class="glyphicon glyphicon-ok-circle" id="daily-dash-glyph"></span>');
 				}
-				if(today_number-eat_fruit_count === 0){
-					$('#eat-fruit-count-no').append('<span class="glyphicon glyphicon-remove-circle" id="daily-dash-glyph-hidden"></span>');
-				}
+				// if(today_number-eat_fruit_count === 0){
+				// 	$('#eat-fruit-count-no').append('<span class="glyphicon glyphicon-remove-circle" id="daily-dash-glyph-hidden"></span>');
+				// }
 				for(var x = 0; x<(today_number-eat_fruit_count); x++){
-					$('#eat-fruit-count-no').append('<span class="glyphicon glyphicon-remove-circle" id="daily-dash-glyph"></span>');
+					$('#eat-fruit-count').append('<span class="glyphicon glyphicon-remove-circle" id="daily-dash-glyph"></span>');
 				}
 				for(var x = 0; x<(7-today_number); x++){
 					$('#eat-fruit-count').append('<span class="glyphicon glyphicon-question-sign" id="daily-dash-glyph"></span>');
 				}
 				if(data['plans'][i]['stats']['streak'] > 0){
-					$('#eat-fruit-today').append('<span class="glyphicon glyphicon-ok" id="daily-dash-glyph"></span>');
+					$('#eat-fruit-today').append('<span class="glyphicon glyphicon-ok"></span>');
 				} else {
-					$('#eat-fruit-today').append('<span class="glyphicon glyphicon-remove" id="daily-dash-glyph"></span>');
+					$('#eat-fruit-today').append('<span class="glyphicon glyphicon-remove"></span>');
 				}
 			} else {
 
